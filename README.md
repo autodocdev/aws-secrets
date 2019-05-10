81097bacc8659f9904b1a5b070f2b04050780bdf
Danilo Correa <dcorrea@autodoc.com.br>
2019-05-10 14:41:24 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# aws-secrets
-Gera um arquivo .env com base no Secret Mananger - Úitl para CI
+# Docker AWS Secrets
+
+Wrapper do AWS Secrets Manager. íra simplemente dar uma saida dos segredos. Útil para carregar
+segredos em um arquivo .env ou exportar no ambiente, por exemplo, em estágios de CI.
+
+Uso básico
+
+```
+$ docker run \
+  -e AWS_SECRET_ACCESS_KEY \
+  -e AWS_ACCESS_KEY_ID \
+  autodoc/awssecrets --region eu-west-1 --secret nameOfSecret
+
+SUPER_SECRET=toto
+ULTRA_SECRET=tata
+VERY_VERY_SECRET=poepoe
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+const AWS = require("aws-sdk");
+const argv = require("minimist")(process.argv.slice(2));
+
+const {
+    secret,
+    region = "eu-west-1",
+    AWS_ACCESS_KEY_ID: accessKeyId,
+    AWS_SECRET_ACCESS_KEY: secretAccessKey,
+} = argv;
+
+const SecretId = secret;
+const params = { region };
+
+if (accessKeyId) { 
+    Object.assign(params, { accessKeyId }); 
+}
+
+if (secretAccessKey) { 
+    Object.assign(params, { secretAccessKey });
+}
+
+const client = new AWS.SecretsManager(params);
+
+if (!secret) {
+    console.log("Missing SECRET_NAME or SECRET_ARN");
+    process.exit(1);
+}
+
+client.getSecretValue({ SecretId }, (err, data) => {
+    if (err) {
+        console.log(`${err.name}: ${err.message}`);
+        process.exit(1);
+    }
+
+    if ("SecretString" in data) {
+        try {
+            const secrets = JSON.parse(data.SecretString);
+            const keys = Object.keys(secrets);
+            keys.forEach(k => {
+                console.log(`${k}=${secrets[k]}`);
+            });
+        }
+        catch (e) {
+            console.log(data.SecretString);
+        }
+    } else {
+    // secret is a binary
+        let buff = new Buffer(data.SecretBinary, "base64");
+        console.log(buff.toString("ascii"));
+    }
+});
